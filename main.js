252dfb2d61092142ead3bff9cb78ffc3496883a0
Paulo Sergio <pauloserg861@gmail.com>
2024-04-14 23:30:33 -0300

    feat: implement functionality to send email

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const label = document.querySelectorAll('input')
-const year = document.querySelector('#year')
-let slideBorder = document.querySelector('.slide-controls')
+const label = document.querySelectorAll('input');
+const year = document.querySelector('#year');
+const showEmailModal = document.querySelector('#showMailModal');
+const emailModal = document.querySelector('.send_email');
+let slideBorder = document.querySelector('.slide-controls');
 
 label.forEach(input => input.addEventListener('click', (e) => {
   let target = e.target.id
@@ -17,3 +19,13 @@ label.forEach(input => input.addEventListener('click', (e) => {
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
 
+// show email modal
+showEmailModal.addEventListener('click', () => {
+  emailModal.classList.add('isVisible')
+})
+
+window.addEventListener('click', (e) => {
+  if(e.target === emailModal) {
+    emailModal.classList.remove('isVisible')
+  }
+})
